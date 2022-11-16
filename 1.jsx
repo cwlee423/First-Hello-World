df875b453e98154c1f396ee2c485aedbc9b0e694
Changwoo Lee <changwoolee@Changwooui-MacBookAir.local>
2022-11-16 11:48:48 +0900

    second commit

--- 1.jsx
+++ 1.jsx
@@ -1,1 +1,11 @@
-RTCSctpTransport
+class a extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( a );
+    }
+}
+ 
+export default a;
